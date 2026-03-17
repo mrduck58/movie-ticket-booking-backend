bea4459c15863ea603c17b2cdbd421877aafdf79
Phong <phongdvhe181343@fpt.edu.vn>
2026-03-17 09:38:00 +0700

    #10 - add payment method api

---
+++ Repositories/Implementations/Payments/PaymentMethodRepository.cs
@@ -0,0 +1,44 @@
+﻿using Movie_Ticket_Booking_Backend.Data;
+using Movie_Ticket_Booking_Backend.Domain.Payments;
+using Movie_Ticket_Booking_Backend.Repositories.Interfaces.Payments;
+using Microsoft.EntityFrameworkCore;
+
+namespace Movie_Ticket_Booking_Backend.Repositories.Implementations.Payments
+{
+    public class PaymentMethodRepository : IPaymentMethodRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PaymentMethodRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PaymentMethod>> GetAllAsync()
+        {
+            return await _context.PaymentMethods.ToListAsync();
+        }
+
+        public async Task<PaymentMethod?> GetByIdAsync(string id)
+        {
+            return await _context.PaymentMethods
+                .FirstOrDefaultAsync(p => p.PaymentMethodId == id);
+        }
+
+        public async Task AddAsync(PaymentMethod method)
+        {
+            await _context.PaymentMethods.AddAsync(method);
+        }
+
+        public async Task DeleteAsync(PaymentMethod method)
+        {
+            _context.PaymentMethods.Remove(method);
+            await Task.CompletedTask;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/Interfaces/Payments/IPaymentMethodService.cs
@@ -0,0 +1,17 @@
+﻿using Movie_Ticket_Booking_Backend.DTOs.Payment;
+
+namespace Movie_Ticket_Booking_Backend.Services.Interfaces.Payments
+{
+    public interface IPaymentMethodService
+    {
+        Task<List<PaymentMethodDto>> GetAll();
+
+        Task<PaymentMethodDto> GetById(string id);
+
+        Task Create(CreatePaymentMethodDto dto);
+
+        Task Update(string id, UpdatePaymentMethodDto dto);
+
+        Task Delete(string id);
+    }
+}
