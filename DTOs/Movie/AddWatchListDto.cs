2a490ed167855e18f0c35fe500ee4a9b4d30b266
Phong <phongdvhe181343@fpt.edu.vn>
2026-03-17 09:37:01 +0700

    #10 - add watchlist api

---
+++ DTOs/Movie/AddWatchListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie_Ticket_Booking_Backend.DTOs.Movie
+{
+    public class AddWatchListDto
+    {
+        public string UserId { get; set; }
+
+        public string MovieId { get; set; }
+
+        public string type { get; set; }
+    }
+}
---
+++ Services/Interfaces/Movies/IWatchListService.cs
@@ -0,0 +1,13 @@
+﻿using Movie_Ticket_Booking_Backend.DTOs.Movie;
+
+namespace Movie_Ticket_Booking_Backend.Services.Interfaces.Movies
+{
+    public interface IWatchListService
+    {
+        Task AddAsync(AddWatchListDto dto);
+
+        Task<List<WatchListDto>> GetUserWatchList(string userId);
+
+        Task RemoveAsync(string userId, string movieId);
+    }
+}
