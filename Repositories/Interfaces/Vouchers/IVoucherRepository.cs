0b9bf6e72832dc466d1971b4cefd31cb1bcfe056
mrduck58 <huanvit0508@gmail.com>
2026-03-25 19:38:50 +0700

    #32 - update new migration, update voucher table

--- Repositories/Interfaces/Vouchers/IVoucherRepository.cs
+++ Repositories/Interfaces/Vouchers/IVoucherRepository.cs
@@ -5,9 +5,7 @@ namespace Movie_Ticket_Booking_Backend.Repositories.Interfaces.Vouchers
     public interface IVoucherRepository
     {
         Task<List<UserVoucher>> GetUserVouchers(string userId);
-
         Task<UserVoucher?> GetUserVoucher(string userId, string voucherId);
-
         Task UpdateAsync(UserVoucher userVoucher);
     }
 }
